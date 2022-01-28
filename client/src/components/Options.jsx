b14d8ceace0b08c543f38fdf396b1eac6fff6008
mirna36 <69789618+mirna36@users.noreply.github.com>
2022-01-28 17:31:57 +0100

    options en option

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Options = ({children}) => {
     return (
         <div>
-            Options
+            Option
+            
             {children}
     
         </div>
